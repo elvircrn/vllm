6b526511dc8b4cd347bdb8ebea54c63700e77678
elvircrn <elvircrn@gmail.com>
2025-08-18 12:21:15 +0000

    Parallel search 1/2

--- benchmarks/kernels/deepgemm/benchmark_grouped_fp8_block_dense_gemm.py
+++ benchmarks/kernels/deepgemm/benchmark_grouped_fp8_block_dense_gemm.py
@@ -469,6 +469,7 @@ def run_benchmarks(verbose: bool = False):
         (4096, 4096, 7168),
         (4096, 7168, 2048),
     ]
+    shapes = shapes[:1]
     num_experts = [8, 40]
     topks = [1, 6, 8]
 
--- csrc/quantization/fp8/per_token_group_quant.cu
+++ csrc/quantization/fp8/per_token_group_quant.cu
@@ -243,21 +243,37 @@ __global__ void per_token_group_quant_8bit_kernel_fused(
       static_cast<float*>(
           output_s)[expert_offset_scaled + col_id * num_tokens + t] = y_s;
     }
-  } else if (half_lane_id == 0) {
-    int32_t expert_idx = 0;
+  } else {
+    int32_t _expert_idx = threadIdx.x % threads_per_group;
     int32_t expert_offset_scaled = 0;
 
+    int32_t expert_idx_base = 0;
     // Let's not touch any memory if we don't need to.
-    for (; expert_idx < num_experts - 1 &&
-           (s_expert_offsets_scaled[expert_idx + 1]) <= scale_id;
-         expert_idx++) {
+    for (; _expert_idx < num_experts - 1 &&
+           (s_expert_offsets_scaled[_expert_idx + 1]) <= scale_id;
+         _expert_idx += threads_per_group) {
+      expert_idx_base += threads_per_group;
     }
-    expert_offset_scaled = s_expert_offsets_scaled[expert_idx];
-    auto num_tokens = (s_expert_offsets_scaled[expert_idx + 1] - s_expert_offsets_scaled[expert_idx]) / k_scaled;
-    int32_t local_id = scale_id - expert_offset_scaled;
-    auto t = local_id / k_scaled;  // Untransposed row.
-    static_cast<float*>(
-        output_s)[expert_offset_scaled + col_id * num_tokens + t] = y_s;
+
+    bool pred = (_expert_idx < num_experts - 2) && s_expert_offsets_scaled[_expert_idx] <= scale_id &&
+           scale_id < s_expert_offsets_scaled[_expert_idx + 1];
+
+    auto predicate_mask = __ballot_sync(0xffffffffu, pred);
+
+    predicate_mask = (predicate_mask >> ((local_group_id & 0b1u) * 16u)) & 0xffffu;
+
+    auto expert_idx = __ffs(predicate_mask) - 1;
+
+    if (half_lane_id == expert_idx && predicate_mask) {
+      _expert_idx = (_expert_idx / threads_per_group) * threads_per_group + expert_idx;
+      expert_offset_scaled = s_expert_offsets_scaled[_expert_idx];
+
+      auto num_tokens = (s_expert_offsets_scaled[_expert_idx + 1] - expert_offset_scaled) / k_scaled;
+      int32_t local_id = scale_id - expert_offset_scaled;
+      auto t = local_id / k_scaled;  // Untransposed row.
+      static_cast<float*>(
+          output_s)[expert_offset_scaled + col_id * num_tokens + t] = y_s;
+    }
   }
 }
 
